65cfe188788448e4b6b27308482c1e9764b95ff4
Andreas Müller <mail@devmount.de>
2016-08-30 18:38:18 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Configuration
+ */
+return [
+
+    /**
+     * Unique name that identifies your theme.
+     */
+    'name' => 'theme-phantom',
+
+    /**
+     * Define menu positions.
+     * Will be listed in the backend so that users
+     * can assign menus to these positions.
+     */
+    'menus' => [
+
+        'main' => 'Main',
+
+    ],
+
+    /**
+     * Define widget positions.
+     * will be listed in the backend so that users
+     * can publish widgets in these positions.
+     */
+    'positions' => [
+
+        'sidebar' => 'Sidebar',
+
+    ],
+
+    /**
+     * Define theme configuration.
+     * This is the theme's default configuration. During run-time, they will be merged with
+     * settings the user has set in the backend. The default configuration can therefore
+     * be overwritten.
+     */
+    'config' => []
+
+];
---
+++ views/template.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= $view->render('head') ?>
+        <?php $view->style('theme', 'theme:css/theme.css') ?>
+        <?php $view->script('theme', 'theme:js/theme.js') ?>
+    </head>
+    <body>
+
+        <!-- Render logo or title with site URL -->
+        <a href="<?= $view->url()->get() ?>">
+            <?php if ($logo = $params['logo']) : ?>
+                <img src="<?= $this->escape($logo) ?>" alt="">
+            <?php else : ?>
+                <?= $params['title'] ?>
+            <?php endif ?>
+        </a>
+
+        <!-- Render menu position -->
+        <?php if ($view->menu()->exists('main')) : ?>
+            <?= $view->menu('main') ?>
+        <?php endif ?>
+
+        <!-- Render widget position -->
+        <?php if ($view->position()->exists('sidebar')) : ?>
+            <?= $view->position('sidebar') ?>
+        <?php endif; ?>
+
+        <!-- Render content -->
+        <?= $view->render('content') ?>
+
+        <!-- Insert code before the closing body tag  -->
+        <?= $view->render('footer') ?>
+
+    </body>
+</html>
