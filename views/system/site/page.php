67285f20cde37ba1ace81aecb452b1b7cb156815
Andreas Müller <mail@devmount.de>
2016-09-02 15:03:21 +0200

    added: page template comments

--- views/system/site/page.php
+++ views/system/site/page.php
@@ -1,9 +1,12 @@
+<!-- show page title except for landing page with title 'Home'  -->
 <?php if ($page->title != 'Home') { ?>
 	<h1><?= $page->title ?></h1>
 <?php } ?>
 
+<!-- show title image if set in page meta section -->
 <?php if (array_key_exists('og:image', $app['node']->data['meta'])) { ?>
 	<span class="image main"><img src="<?= $app['node']->data['meta']['og:image'] ?>" alt="" /></span>
 <?php } ?>
 
+<!-- show page content -->
 <?= $page->content ?>
